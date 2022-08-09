e3a8496e587f7c03dd62770a5aaeb63bcee24425
joseMarciano <marcianojosepaulo@gmail.com>
2022-08-09 17:19:27 -0300

    feat: :sparkles: create a implementation for base class domain

--- domain/src/main/java/com/delivery/system/domain/AggregateRoot.java
+++ domain/src/main/java/com/delivery/system/domain/AggregateRoot.java
@@ -1,4 +1,8 @@
 package com.delivery.system.domain;
 
 public abstract class AggregateRoot<ID extends Identifier> extends Entity<ID> {
+
+    public AggregateRoot(final ID id) {
+        super(id);
+    }
 }
