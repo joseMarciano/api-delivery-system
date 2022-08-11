d4d367e1cd3cd3bc956ce234ba901ba94a99d9f3
joseMarciano <marcianojosepaulo@gmail.com>
2022-08-10 23:12:51 -0300

    feat: :sparkles: implements equals and hashcode in DriverID

--- domain/src/main/java/com/delivery/system/domain/driver/DriverID.java
+++ domain/src/main/java/com/delivery/system/domain/driver/DriverID.java
@@ -2,6 +2,7 @@ package com.delivery.system.domain.driver;
 
 import com.delivery.system.domain.Identifier;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public class DriverID extends Identifier {
@@ -13,12 +14,30 @@ public class DriverID extends Identifier {
     }
 
     private DriverID(String aValue) {
+        Objects.requireNonNull(aValue);
         this.value = aValue;
+    }
+
+    public static DriverID from(final String anId) {
+        return new DriverID(anId);
     }
 
 
     @Override
     public String getValue() {
         return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final var driverID = (DriverID) o;
+        return getValue().equals(driverID.getValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getValue());
     }
 }
