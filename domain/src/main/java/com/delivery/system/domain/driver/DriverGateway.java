a821c3577ddf9c36ade0347bf7368a0c47b61adc
joseMarciano <marcianojosepaulo@gmail.com>
2022-08-10 23:14:56 -0300

    feat: :sparkles: create updade and findById methods in DriverGateway

--- domain/src/main/java/com/delivery/system/domain/driver/DriverGateway.java
+++ domain/src/main/java/com/delivery/system/domain/driver/DriverGateway.java
@@ -1,7 +1,13 @@
 package com.delivery.system.domain.driver;
 
+import java.util.Optional;
+
 public interface DriverGateway {
 
     Driver create(Driver aDriver);
+
+    Driver update(Driver aDriver);
+
+    Optional<Driver> findById(DriverID anId);
 
 }
