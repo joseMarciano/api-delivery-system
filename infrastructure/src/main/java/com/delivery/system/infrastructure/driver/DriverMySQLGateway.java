a31970e04673edd71a83d97a10d5f9fd8ab235ec
joseMarciano <marcianojosepaulo@gmail.com>
2022-08-13 11:52:30 -0300

    feat: :sparkles: create DriverMySQLGateway without implementations

---
+++ infrastructure/src/main/java/com/delivery/system/infrastructure/driver/DriverMySQLGateway.java
@@ -0,0 +1,45 @@
+package com.delivery.system.infrastructure.driver;
+
+import com.delivery.system.domain.driver.Driver;
+import com.delivery.system.domain.driver.DriverGateway;
+import com.delivery.system.domain.driver.DriverID;
+import com.delivery.system.infrastructure.driver.persistence.DriverRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class DriverMySQLGateway implements DriverGateway {
+
+    private final DriverRepository driverRepository;
+
+    public DriverMySQLGateway(DriverRepository driverRepository) {
+        this.driverRepository = driverRepository;
+    }
+
+    @Override
+    public Driver create(Driver aDriver) {
+        return null;
+    }
+
+    @Override
+    public Driver update(Driver aDriver) {
+        return null;
+    }
+
+    @Override
+    public Optional<Driver> findById(DriverID anId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void deleteById(DriverID anId) {
+
+    }
+
+    @Override
+    public List<Driver> findAll() {
+        return null;
+    }
+}
