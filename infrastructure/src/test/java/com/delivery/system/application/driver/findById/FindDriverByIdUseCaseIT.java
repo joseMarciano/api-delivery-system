64e9ee20bf242d737841587916bfaf4d66f206c3
joseMarciano <marcianojosepaulo@gmail.com>
2022-08-14 11:31:54 -0300

    test: :test_tube: implements FindDriverByIdUseCase integration test

---
+++ infrastructure/src/test/java/com/delivery/system/application/driver/findById/FindDriverByIdUseCaseIT.java
@@ -0,0 +1,58 @@
+package com.delivery.system.application.driver.findById;
+
+import com.delivery.system.IntegrationTest;
+import com.delivery.system.domain.driver.Driver;
+import com.delivery.system.domain.driver.DriverID;
+import com.delivery.system.domain.exceptions.NotFoundException;
+import com.delivery.system.infrastructure.driver.persistence.DriverJpaEntity;
+import com.delivery.system.infrastructure.driver.persistence.DriverRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@IntegrationTest
+public class FindDriverByIdUseCaseIT {
+
+    @Autowired
+    private FindDriverByIdUseCase useCase;
+
+    @Autowired
+    private DriverRepository driverRepository;
+
+
+    @Test
+    public void givenAValidCommand_whenCallsFindById_shouldReturnADriver() {
+        final var expectedName = "John";
+        final var aDriver = Driver.newDriver(expectedName);
+        final var expectedId = aDriver.getId();
+
+        assertEquals(0, driverRepository.count());
+        driverRepository.saveAndFlush(DriverJpaEntity.from(aDriver));
+        assertEquals(1, driverRepository.count());
+
+        final var aCommand = FindDriverByIdCommand.with(expectedId.getValue());
+
+        final var actualDriver = useCase.execute(aCommand);
+
+        assertEquals(actualDriver.name(), expectedName);
+        assertEquals(actualDriver.id(), expectedId.getValue());
+        assertNotNull(actualDriver.updatedAt());
+        assertNotNull(actualDriver.createdAt());
+    }
+
+    @Test
+    public void givenAInvalidCommandWithNoExistingDriver_whenCallsFindById_shouldReturnNotFoundException() {
+        final var expectedId = DriverID.unique();
+
+        final var expectedErrorMessage = "Driver with identifier %s was not found".formatted(expectedId.getValue());
+
+        assertEquals(0, driverRepository.count());
+        final var aCommand = FindDriverByIdCommand.with(expectedId.getValue());
+
+        final var actualOuput = assertThrows(NotFoundException.class, () -> useCase.execute(aCommand));
+
+        assertEquals(actualOuput.getMessage(), expectedErrorMessage);
+        assertEquals(0, driverRepository.count());
+    }
+}
