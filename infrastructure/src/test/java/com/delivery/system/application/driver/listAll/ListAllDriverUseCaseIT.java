5c6d66ea9634cb1e7135b35f9017a00ddc8f16ac
joseMarciano <marcianojosepaulo@gmail.com>
2022-08-14 11:52:59 -0300

    test: :test_tube: implements ListAllDriverUseCase integration test

---
+++ infrastructure/src/test/java/com/delivery/system/application/driver/listAll/ListAllDriverUseCaseIT.java
@@ -0,0 +1,62 @@
+package com.delivery.system.application.driver.listAll;
+
+import com.delivery.system.IntegrationTest;
+import com.delivery.system.application.driver.retrieve.listAll.ListAllDriverOutput;
+import com.delivery.system.application.driver.retrieve.listAll.ListAllDriverUseCase;
+import com.delivery.system.domain.driver.Driver;
+import com.delivery.system.infrastructure.driver.persistence.DriverJpaEntity;
+import com.delivery.system.infrastructure.driver.persistence.DriverRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@IntegrationTest
+public class ListAllDriverUseCaseIT {
+
+    @Autowired
+    private ListAllDriverUseCase useCase;
+
+    @Autowired
+    private DriverRepository driverRepository;
+
+    @Test
+    public void givenAValidScenario_whenCallsListAllDriver_shouldReturnAListOfDrivers() {
+        final var expectedDrivers =
+                List.of(
+                        Driver.newDriver("John"),
+                        Driver.newDriver("Amanda")
+                );
+
+        assertEquals(0, driverRepository.count());
+        driverRepository.saveAllAndFlush(mapTo(expectedDrivers, DriverJpaEntity::from));
+        assertEquals(2, driverRepository.count());
+
+        final var actualOuput = useCase.execute();
+
+        assertTrue(
+                actualOuput.size() == expectedDrivers.size()
+                        && mapTo(expectedDrivers, ListAllDriverOutput::from).containsAll(actualOuput)
+        );
+    }
+
+    @Test
+    public void givenAValidScenarioWhereDriversIsEmpty_whenCallsListAllDriver_shouldReturnAEmptyList() {
+        final var expectedSizeList = 0;
+
+        assertEquals(0, driverRepository.count());
+        final var actualOuput = useCase.execute();
+        assertEquals(0, driverRepository.count());
+
+        assertEquals(expectedSizeList, actualOuput.size());
+    }
+
+    private <I, O> List<O> mapTo(List<I> list, Function<I, O> mapper) {
+        return list.stream().map(mapper).toList();
+    }
+
+}
