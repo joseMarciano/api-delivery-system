770269123cdd1fc70ee1185024715c244ffd493f
joseMarciano <marcianojosepaulo@gmail.com>
2022-08-16 23:14:59 -0300

    fix: :bug: makes E2E work correctly with MockMvc

--- infrastructure/src/test/java/com/delivery/system/E2ETest.java
+++ infrastructure/src/test/java/com/delivery/system/E2ETest.java
@@ -1,20 +1,18 @@
 package com.delivery.system;
 
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.lang.annotation.*;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("e2e-test")
 @ExtendWith(value = {PostgreContainerExtension.class, MYSQLGatewayCleanUpExtension.class})
+@AutoConfigureMockMvc
 public @interface E2ETest {
-
 }
