4ce339d07cea4c090ad4d906e4727f7cf50ecce8
joseMarciano <marcianojosepaulo@gmail.com>
2022-08-20 17:32:36 -0300

    fix: :bug: add test containers on E2E test correctly

--- infrastructure/src/test/java/com/delivery/system/infrastructure/api/driver/DriverControllerTestE2E.java
+++ infrastructure/src/test/java/com/delivery/system/infrastructure/api/driver/DriverControllerTestE2E.java
@@ -10,14 +10,37 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @E2ETest
+@Testcontainers
 public class DriverControllerTestE2E {
 
     private static final String BASE_PATH = "/drivers";
+
+    @Container
+    public static PostgreSQLContainer<?> postgresDB =
+            new PostgreSQLContainer<>("postgres:14.5")
+                    .withDatabaseName("order_manager")
+                    .withUsername("order_owner")
+                    .withPassword("a1b2c3f4e5");
+
+
+    @DynamicPropertySource
+    public static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresDB::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresDB::getUsername);
+        registry.add("spring.datasource.password", postgresDB::getPassword);
+        registry.add("spring.datasource.port", () -> postgresDB.getMappedPort(5432));
+    }
+
 
     @Autowired
     private MockMvc mvc;
@@ -65,12 +88,5 @@ public class DriverControllerTestE2E {
         Assertions.assertEquals(actualResponseBody.createdAt(), actualEntity.getCreatedAt());
 
 
-    }
-
-    @Test
-    public void testando2() {
-//        Assertions.assertEquals(0, driverRepository.count());
-//        driverRepository.save(DriverJpaEntity.from(Driver.newDriver("John")));
-//        Assertions.assertEquals(1, driverRepository.count());
     }
 }
