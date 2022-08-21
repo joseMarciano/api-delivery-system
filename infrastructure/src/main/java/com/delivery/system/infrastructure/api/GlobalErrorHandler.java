08b5b620a5a711e2a622b2f0ec2cecaec32189a9
joseMarciano <marcianojosepaulo@gmail.com>
2022-08-21 20:17:07 -0300

    feat: :sparkles: create GlobalErrorHandler for api

---
+++ infrastructure/src/main/java/com/delivery/system/infrastructure/api/GlobalErrorHandler.java
@@ -0,0 +1,28 @@
+package com.delivery.system.infrastructure.api;
+
+
+import com.delivery.system.domain.exceptions.DomainException;
+import com.delivery.system.domain.validation.Error;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+
+@ControllerAdvice
+public class GlobalErrorHandler {
+
+    @ExceptionHandler(value = {DomainException.class})
+    public ResponseEntity<?> domainExceptionHandler(final DomainException domainException) {
+        final var apiError = ApiError
+                .with(domainException.getMessage(), domainException.getErrors());
+        return ResponseEntity.unprocessableEntity().body(apiError);
+    }
+
+    record ApiError(String message, List<Error> errors) {
+        public static ApiError with(final String message, final List<Error> errors) {
+            return new ApiError(message, errors);
+        }
+
+    }
+}
