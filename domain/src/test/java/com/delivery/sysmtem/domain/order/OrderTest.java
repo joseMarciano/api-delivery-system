0bf530e60519c8d713e3514050c7ef56f43bf2d3
joseMarciano <marcianojosepaulo@gmail.com>
2022-08-24 22:52:21 -0300

    test: :test_tube: create changeStatus order test

--- domain/src/test/java/com/delivery/sysmtem/domain/order/OrderTest.java
+++ domain/src/test/java/com/delivery/sysmtem/domain/order/OrderTest.java
@@ -84,29 +84,82 @@ public class OrderTest {
 
     @Test
     public void givenANullName_whenCallUpdate_thenThrowsDomainException() {
-        final String expectedName = null;
+        final String expectedDescription = null;
         final var expectedDriverId = DriverID.unique();
 
         final var expectedErrorMessage = "'description' should not be null";
         final var aOrder = Order.newOrder("pacage", expectedDriverId);
 
         final var actualException =
-                assertThrows(DomainException.class, () -> aOrder.update(expectedName, expectedDriverId));
+                assertThrows(DomainException.class, () -> aOrder.update(expectedDescription, expectedDriverId));
 
         assertEquals(expectedErrorMessage, actualException.getMessage());
     }
 
     @Test
     public void givenANullDriverID_whenCallUpdate_thenThrowsDomainException() {
-        final var expectedName = "Package";
+        final var expectedDescription = "Package";
         final DriverID expectedDriverId = null;
 
         final var expectedErrorMessage = "'driverId' should not be null";
-        final var aOrder = Order.newOrder(expectedName, DriverID.unique());
+        final var aOrder = Order.newOrder(expectedDescription, DriverID.unique());
 
         final var actualException =
-                assertThrows(DomainException.class, () -> aOrder.update(expectedName, expectedDriverId));
+                assertThrows(DomainException.class, () -> aOrder.update(expectedDescription, expectedDriverId));
 
         assertEquals(expectedErrorMessage, actualException.getMessage());
     }
+
+    @Test
+    public void givenAValidParam_whenCallsChangeStatusInProgress_thenReturnOrderStatusChanged() {
+        final var expectedDescription = "Package";
+        final var expectedDriverId = DriverID.unique();
+        final var expectedStatus = StatusOrder.IN_PROGRESS;
+
+        final var aOrder = Order.newOrder(expectedDescription, expectedDriverId);
+
+        final var actualOrder = aOrder.changeStatus(expectedStatus);
+
+        assertEquals(expectedDescription, actualOrder.getDescription());
+        assertEquals(expectedStatus, actualOrder.getStatus());
+        assertNotNull(actualOrder.getCreatedAt());
+        assertNotNull(actualOrder.getUpdatedAt());
+        assertTrue(actualOrder.getCreatedAt().isBefore(actualOrder.getUpdatedAt()));
+        assertNull(actualOrder.getTimeDelivered());
+    }
+
+    @Test
+    public void givenAValidParam_whenCallsChangeStatusDelivered_thenReturnOrderStatusChanged() {
+        final var expectedDescription = "Package";
+        final var expectedDriverId = DriverID.unique();
+        final var expectedStatus = StatusOrder.DELIVERED;
+
+        final var aOrder = Order.newOrder(expectedDescription, expectedDriverId);
+
+        final var actualOrder = aOrder.changeStatus(expectedStatus);
+
+        assertEquals(expectedDescription, actualOrder.getDescription());
+        assertEquals(expectedStatus, actualOrder.getStatus());
+        assertNotNull(actualOrder.getCreatedAt());
+        assertNotNull(actualOrder.getUpdatedAt());
+        assertTrue(actualOrder.getCreatedAt().isBefore(actualOrder.getUpdatedAt()));
+        assertNotNull(actualOrder.getTimeDelivered());
+    }
+
+    @Test
+    public void givenAInvalidParam_whenCallsChangeStatusWithNullValue_shouldReturnDomainException() {
+        final var expectedDescription = "Package";
+        final var expectedDriverId = DriverID.unique();
+        final StatusOrder expectedStatus = null;
+
+        final var expectedErrorMessage = "'statusOrder' should not be null";
+
+        final var aOrder = Order.newOrder(expectedDescription, expectedDriverId);
+
+        final var actualException =
+                assertThrows(DomainException.class, () -> aOrder.changeStatus(expectedStatus));
+
+        assertEquals(expectedErrorMessage, actualException.getMessage());
+    }
+
 }
