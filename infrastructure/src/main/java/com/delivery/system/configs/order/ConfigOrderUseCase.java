9f0372755fb358353c70fb17d8aedd2b9b24697c
joseMarciano <marcianojosepaulo@gmail.com>
2022-08-30 21:16:17 -0300

    chore: :wrench: create configuration Bean for CreateOrderUseCase

---
+++ infrastructure/src/main/java/com/delivery/system/configs/order/ConfigOrderUseCase.java
@@ -0,0 +1,22 @@
+package com.delivery.system.configs.order;
+
+import com.delivery.system.application.order.create.CreateOrderUseCase;
+import com.delivery.system.application.order.create.DefaultCreateOrderUseCase;
+import com.delivery.system.domain.order.OrderGateway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigOrderUseCase {
+
+    private final OrderGateway orderGateway;
+
+    public ConfigOrderUseCase(OrderGateway orderGateway) {
+        this.orderGateway = orderGateway;
+    }
+
+    @Bean
+    public CreateOrderUseCase createOrderUseCase() {
+        return new DefaultCreateOrderUseCase(orderGateway);
+    }
+}
