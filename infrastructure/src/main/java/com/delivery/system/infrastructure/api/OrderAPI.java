6975f019f172d7c34c5948e1e795b9b73bb8d64f
joseMarciano <marcianojosepaulo@gmail.com>
2022-08-30 23:09:54 -0300

    chore: :wrench: create OrderAPI

---
+++ infrastructure/src/main/java/com/delivery/system/infrastructure/api/OrderAPI.java
@@ -0,0 +1,25 @@
+package com.delivery.system.infrastructure.api;
+
+import com.delivery.system.infrastructure.order.models.create.CreateOrderRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/orders")
+public interface OrderAPI {
+
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseStatus(HttpStatus.CREATED)
+    void create(@RequestBody CreateOrderRequest createOrderRequest);
+
+    @GetMapping(
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    List<?> create();
+
+
+}
