9c53f2a7063233d3964cc34a6c23e2b3d8c6cf37
joseMarciano <marcianojosepaulo@gmail.com>
2022-08-30 23:19:26 -0300

    refactor: :hammer: add more props on output list

--- application/src/main/java/com/delivery/system/application/order/retrieve/listAll/ListAllOrdersOutput.java
+++ application/src/main/java/com/delivery/system/application/order/retrieve/listAll/ListAllOrdersOutput.java
@@ -1,12 +1,21 @@
 package com.delivery.system.application.order.retrieve.listAll;
 
 import com.delivery.system.domain.order.Order;
+import com.delivery.system.domain.order.StatusOrder;
+
+import java.time.Instant;
 
 public record ListAllOrdersOutput(
         String id,
-        String description
+        String description,
+        StatusOrder statusOrder,
+        Instant deliveredAt
 ) {
     public static ListAllOrdersOutput from(final Order anOrder) {
-        return new ListAllOrdersOutput(anOrder.getId().getValue(), anOrder.getDescription());
+        return new ListAllOrdersOutput(
+                anOrder.getId().getValue(),
+                anOrder.getDescription(),
+                anOrder.getStatus(),
+                anOrder.getTimeDelivered());
     }
 }
