3599a21c5456fb3d5d46cb9e46a65bcea112fa5c
joseMarciano <marcianojosepaulo@gmail.com>
2022-09-03 18:08:41 -0300

    feat: :sparkles: create orderDeliveredListener listeners

---
+++ infrastructure/src/main/java/com/delivery/system/infrastructure/listeners/order/RabbitMqOrderListener.java
@@ -0,0 +1,29 @@
+package com.delivery.system.infrastructure.listeners.order;
+
+import com.delivery.system.application.order.updateStatus.UpdateStatusOrderCommand;
+import com.delivery.system.application.order.updateStatus.UpdateStatusOrderUseCase;
+import com.delivery.system.configs.json.Json;
+import com.delivery.system.domain.order.StatusOrder;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitMqOrderListener {
+
+
+    private final UpdateStatusOrderUseCase updateStatusOrderUseCase;
+
+    public RabbitMqOrderListener(final UpdateStatusOrderUseCase updateStatusOrderUseCase) {
+        this.updateStatusOrderUseCase = updateStatusOrderUseCase;
+    }
+
+    @RabbitListener(id = "order.delivered", queues = "order.delivered")
+    public void orderDeliveredListener(String anIn) {
+        final var anInput = Json.readValue(anIn, OrderDeliveryListenerInput.class);
+        updateStatusOrderUseCase.execute(UpdateStatusOrderCommand.with(anInput.value(), StatusOrder.DELIVERED));
+    }
+
+//    @RabbitListener(queues = "order.inProgress")
+//    public void orderinProgresListener(String in) {
+//    }
+}
