977bdb4db97635632678394e1acef7add2738a6e
joseMarciano <marcianojosepaulo@gmail.com>
2022-09-07 19:35:45 -0300

    feat: :sparkles: create Queues

--- infrastructure/src/main/java/com/delivery/system/configs/rabbitmq/ConfigRabbitMQ.java
+++ infrastructure/src/main/java/com/delivery/system/configs/rabbitmq/ConfigRabbitMQ.java
@@ -11,4 +11,14 @@ public class ConfigRabbitMQ {
     public Queue orderCreatedQueue() {
         return new Queue("order.created", true, false, false);
     }
+
+    @Bean
+    public Queue orderDeliveredQueue() {
+        return new Queue("order.delivered", true, false, false);
+    }
+
+    @Bean
+    public Queue orderInProgressQueue() {
+        return new Queue("order.inProgress", true, false, false);
+    }
 }
