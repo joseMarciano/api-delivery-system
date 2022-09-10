0826656e17362dbbdbbd7c8b5112993e327cea2a
joseMarciano <marcianojosepaulo@gmail.com>
2022-09-10 17:26:13 -0300

    chore: :wrench: create configuration about CORS

---
+++ infrastructure/src/main/java/com/delivery/system/configs/cors/EnableCors.java
@@ -0,0 +1,15 @@
+package com.delivery.system.configs.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class EnableCors implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**");
+    }
+}
